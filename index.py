445462107cb052076a37e3e6e29e7c530eb83eab
JoaquinMorais <joaquinmorais2005@gmail.com>
2023-01-22 00:04:35 -0300

    probando cambios

--- index.py
+++ index.py
@@ -1,10 +1,20 @@
+"""
 from app import app
 from utils.db import db
 
-"""
+
 with app.app_context():
     db.create_all()
 """
+from flask import Flask
+#from flask_sqlalchemy import SQLAlchemy
+
+from routes.home import Home
+
+app = Flask(__name__)
+app.secret_key = '3594ea6944dfc5dcd8eaadf8dff0759e'
+
+app.register_blueprint(Home)
 
 if __name__=='__main__':
     app.run(debug=True)
