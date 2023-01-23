76ce62fd703f1089577f1640dc26c28a3e1857b9
unknown <joaquinmorais2005@gmail.com>
2023-01-23 02:23:11 -0300

    cambios en el archivo index.py

--- index.py
+++ index.py
@@ -1,8 +1,8 @@
-"""
+
 from app import app
 from utils.db import db
 
-
+"""
 with app.app_context():
     db.create_all()
 """
@@ -10,11 +10,6 @@ from flask import Flask
 #from flask_sqlalchemy import SQLAlchemy
 
 from routes.home import Home
-
-app = Flask(__name__)
-app.secret_key = '3594ea6944dfc5dcd8eaadf8dff0759e'
-
-app.register_blueprint(Home)
 
 if __name__=='__main__':
     app.run(debug=True)
