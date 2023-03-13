158164f821b65d4e291afef158b8e03c5c53ef38
Agus Salonia <agusalonia04@gmail.com>
2023-03-13 20:52:08 -0300

    Lussary y Fermag 84.0

--- static/js/clientes/Fermag/fermag.js
+++ static/js/clientes/Fermag/fermag.js
@@ -1,22 +1,35 @@
-var index = 0;
+var imagenes = ['/static/src/clientes/Fermag/fondo1.png', '/static/src/clientes/Fermag/fondo2.jpeg', '/static/src/clientes/Fermag/fondo3.jpeg'],
+    cont = 0;
 
-    var listaimg = ["/Lussary/Lussary/static/src/clientes/Fermag/fondo1.jpg", "/Lussary/Lussary/static/src/clientes/Fermag/fondo2.jpg", "/Lussary/Lussary/static/src/clientes/Fermag/fondo3.jpg", "/Lussary/Lussary/static/src/clientes/Fermag/fondo4.jpg"];
+function carrousel(inicio){
+    inicio.addEventListener('click', e => {
+        let atras = inicio.querySelector('.atras'),
+            adelante = inicio.querySelector('.adelante'),
+            img = inicio.querySelector('img'),
+            tgt = e.target;
 
-$(function() {
-  
-    setInterval(changeImage, 2000);
-  
-});
-
-function changeImage() {
-  
- 
-   $('inicio-img').img("background-image", 'url(' + listaimg[index] + ')');
-                  
-   index++;
-                  
-   if(index == 4)
-      index = 0;
-    
-    
+        if(tgt == atras){
+            if(cont > 0){
+                img.src = imagenes[cont - 1];
+                cont--;
+            } else {
+                img.src = imagenes[imagenes.length - 1 ];
+                cont = imagenes.length - 1;
+            }
+        } else if(tgt == adelante){
+            if(cont < imagenes.length - 1){
+                img.src = imagenes[cont + 1];
+                cont++;
+            } else {
+                img.src = imagenes[0];
+                cont = 0;
+            }
+        }
+    });
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    let inicio = document.querySelector('.inicio');
+
+    carrousel(inicio);
+});
