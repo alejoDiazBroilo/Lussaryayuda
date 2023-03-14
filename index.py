1dafdcb36e0fc358d10d2644ad90b34c223a66f7
JoaquinMorais <joaquinmorais2005@gmail.com>
2023-03-14 08:34:13 -0300

    wtf amigo

--- index.py
+++ index.py
@@ -1,23 +1,9 @@
-from app import app
-from utils.db import db
-from models.tasks import Task
+from flask import Flask
+app = Flask(__name__)
 
-from routes.lussary_config.settings import Home
-from routes.clientes.VEME.VEME_home import VEME_home
-from routes.clientes.Fermag.Fermag_home import Fermag_home
-
-
-with app.app_context():
-    db.create_all()
-    
-
-app.register_blueprint(Home)
-app.register_blueprint(VEME_home)
-app.register_blueprint(Fermag_home)
+@app.route('/')
+def hello():
+    return 'Hello World!'
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
