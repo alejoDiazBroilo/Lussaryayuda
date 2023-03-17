391e190ed544008a69cb0d75262ec65e6bbfc17d
MarcoFm1 <marco.fini22@gmail.com>
2023-03-17 13:36:47 -0300

    hora, senias

---
+++ static/js/lussary_config/settings_js.js
@@ -0,0 +1,8 @@
+function updateTime() {
+    const now = new Date();
+    const clock = document.getElementById('clock');
+    clock.innerText = now.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
+  }
+  
+setInterval(updateTime, 1000);
+  
