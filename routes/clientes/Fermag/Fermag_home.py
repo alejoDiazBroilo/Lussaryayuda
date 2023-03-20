125f0a454793895e49363116652eb9aa1a692460
MarcoFm1 <marco.fini22@gmail.com>
2023-03-20 11:36:41 -0300

    start

--- routes/clientes/Fermag/Fermag_home.py
+++ routes/clientes/Fermag/Fermag_home.py
@@ -7,3 +7,8 @@ Fermag_home = Blueprint("Fermag_home",__name__)
 @Fermag_home.route("/clientes/Fermag")
 def getPrincipalPageFermag():
     return render_template("clientes/Fermag/fermag.html")
+
+
+@Fermag_home.route("/clientes/Fermag/proyectos")
+def getProjectPageFermag():
+    return render_template("clientes/Fermag/proyectos.html")
