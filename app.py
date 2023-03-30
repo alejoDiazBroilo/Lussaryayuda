86e4123155180c7b001f3091d191bfde02085bd8
JoaquinMorais <joaquinmorais2005@gmail.com>
2023-03-30 11:13:04 -0300

    git ignore añadido

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']= 'mysql+pymysql://root:i3eXbNRZa4GIGFN37K8s@containers-us-west-151.railway.app:5647/railway'
+#app.config['SQLALCHEMY_DATABASE_URI']= 'mysql+pymysql://root:i3eXbNRZa4GIGFN37K8s@containers-us-west-151.railway.app:5647/railway'
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://bdi:pepe1234@localhost/Flask_Utility"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
