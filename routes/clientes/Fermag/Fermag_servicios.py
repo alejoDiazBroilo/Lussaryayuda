5aebdebad225147edd200f3d90f1311e20d01c1f
MarcoFm1 <marco.fini22@gmail.com>
2023-04-04 11:59:08 -0300

    servicios jejeje

--- routes/clientes/Fermag/Fermag_servicios.py
+++ routes/clientes/Fermag/Fermag_servicios.py
@@ -4,8 +4,24 @@ from utils.db import db
 Fermag_servicios = Blueprint("Fermag_servicios",__name__)
 
 
-@Fermag_servicios.route("/clientes/Fermag/servicios")
-def getServicePageFermag():
-    return render_template("clientes/Fermag/servicios.html")
 
+class Servicios:
+    def __init__(self,nombre):
+        self.nombre = nombre
 
+        
+
+    def __str__(self):
+        return f'{self.nombre}'
+
+@Fermag_servicios.route("/clientes/Fermag/servicios")    
+def FermagServicios():
+    infos = [ 
+        Servicios('HOSPITAL PASTEUR'),
+        Servicios('EL PAPA DE SALO'),
+        Servicios('LA MAMA DE SALO'),
+        Servicios('SALO LA MAMA'),
+        Servicios('MUCHI')                
+        ]
+    
+    return render_template("clientes/Fermag/servicios.html", infos=infos)
