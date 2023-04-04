68f591f4df280e0af860b4a7eaa155f8a7871229
Agus Salonia <agusalonia04@gmail.com>
2023-04-04 08:43:20 -0300

    fermag avances

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']= 'mysql+pymysql://root:i3eXbNRZa4GIGFN37K8s@containers-us-west-151.railway.app:5647/railway'
+app.config['SQLALCHEMY_DATABASE_URI']= 'mysql+pymysql://root:9Rtcn2nInmjU8TVIFmg9@containers-us-west-47.railway.app:6674/railway'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
