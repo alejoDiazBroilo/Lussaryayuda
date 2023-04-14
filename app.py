ceffa83ff249b087c7fa14323debf0a3597e8a88
JoaquinMorais <joaquinmorais2005@gmail.com>
2023-04-14 10:34:39 -0300

    arreglando por fin la db

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ app = Flask(__name__)
 CREAR USUARIO CON - NOMBRE:bdi - y - CONTRASEÑA: pepe1234 - y despues escribir lo de abajo en mysql ->
 drop database LussaryTest; create database LussaryTest; use LussaryTest;
 """
-app.config['SQLALCHEMY_DATABASE_URI']= 'mysql+pymysql://root:9Rtcn2nInmjU8TVIFmg9@containers-us-west-47.railway.app:6674/railway'
+app.config['SQLALCHEMY_DATABASE_URI']= 'mysql+pymysql://root:ADzgCFn3et4x6wJh0LIN@containers-us-west-207.railway.app:7459/railway'
 #app.config['SQLALCHEMY_DATABASE_URI']= "mysql+pymysql://bdi:pepe1234@localhost/LussaryTest"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
