ed2b207171309d26878933af5dfd9d6fe48f3236
MarcoFm1 <marco.fini22@gmail.com>
2023-04-20 10:53:39 -0300

    arreglando cosas

--- static/js/lussary_config/settings_js.js
+++ static/js/lussary_config/settings_js.js
@@ -1,6 +1,16 @@
+imagen_logo = document.getElementById('imagen-logo')
+
 function updateTime() {
     const now = new Date();
     const clock = document.getElementById('clock');
     clock.innerText = now.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
   }
   
+
+  window.addEventListener("load", function(){
+    const loader = document.querySelector("#loader-container");
+    setTimeout(function() {
+      loader.className += " hidden";
+    }, 800); 
+  });
+    
