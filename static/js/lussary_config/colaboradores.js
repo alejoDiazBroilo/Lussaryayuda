3aaea5095913750f0815e955a92a14360f4ec488
JoaquinMorais <joaquinmorais2005@gmail.com>
2023-04-28 16:27:56 -0300

    mejorando la imagen

--- static/js/lussary_config/colaboradores.js
+++ static/js/lussary_config/colaboradores.js
@@ -1,9 +1,20 @@
 var content = document.getElementById("content");
+var center_content = document.getElementById("centro_content")
+var difuminado = document.getElementById("difuminado")
+
+
+center_content.addEventListener('mouseover', function() {
+  difuminado.style.backdropFilter = "blur(0)"
+});
+center_content.addEventListener('mouseout', function() {
+  difuminado.style.backdropFilter = "blur(2px)"
+}); /* */
+
 
 setTimeout(() => {
     content.className = "content";
     console.log('Colaboradores')
-  }, "2000");
+  }, "200");
 
   
 
